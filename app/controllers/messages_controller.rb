0ab64493d837dd7d9c1d77aac9f0b7a0a4356be3
ak-2015 <akiko_torii@hotmail.com>
2020-05-10 21:49:07 +0900

    Modified codes - messages_main.haml
    Messages#index is temporary code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,9 @@
 class MessagesController < ApplicationController
+  
   def index
+    @group = Group.find(1)
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
+
 end
