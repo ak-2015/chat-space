834be06852ddfaf29d6056c5f3acdc9574173cd6
ak-2015 <akiko_torii@hotmail.com>
2020-05-11 17:06:08 +0900

    moved #set-group to private action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,13 +30,14 @@ class GroupsController < ApplicationController
     end
   end
 
-  def set_group
-    @group = Group.find(params[:id])
-  end
-
   private
+  
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
   
 end
