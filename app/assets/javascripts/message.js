65e1c62a348839ddee0b31fcec6d9b380179d945
ak-2015 <akiko_torii@hotmail.com>
2020-05-17 15:43:21 +0900

    added function for done case - message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,55 @@
 $(function(){
+  function buildHTML(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message-items">` +
+        `<div class="message-items__title">` +
+        `<div class="member">` +
+        message.member +
+        `</div>` +
+        `<div class="timestamp">` +
+        message.timestamp +
+        `</div>` +
+        `</div>` +
+        `<div class="message-items__message">` +
+        `<p class="content">` +
+        message.content +
+        `</p>` +
+        `<img src="` + message.image + `" class="image" >` +
+        `</div>` +
+        `</div>`
+    } else if (message.content) {
+      var html = `<div class="message-items">` +
+        `<div class="message-items__title">` +
+        `<div class="member">` +
+        message.member +
+        `</div>` +
+        `<div class="timestamp">` +
+        message.timestamp +
+        `</div>` +
+        `</div>` +
+        `<div class="message-items__message">` +
+        `<p class="content">` +
+        message.content +
+        `</p>` +
+        `</div>` +
+        `</div>`
+    } else if (message.image) {
+      var html = `<div class="message-items">` +
+        `<div class="message-items__title">` +
+        `<div class="member">` +
+        message.member +
+        `</div>` +
+        `<div class="timestamp">` +
+        message.timestamp +
+        `</div>` +
+        `</div>` +
+        `<div class="message-items__message">` +
+        `<img src="` + message.image + `" class="image" >` +
+        `</div>` +
+        `</div>`
+    };
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var url = $(this).attr('action');
@@ -11,6 +62,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main').find('.main').append(html);
+      $('form')[0].reset();
     })
   })
 })
