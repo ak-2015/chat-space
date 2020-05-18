5182320b668a9b2ea3758142c295acddf04c37ca
ak-2015 <akiko_torii@hotmail.com>
2020-05-19 00:53:14 +0900

    created function - added user to be deleted from the list

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -42,4 +42,9 @@ $(function() {
       alert("ユーザー検索に失敗しました");
     });
   });
+
+  $(document).on('click', ".chat-group-user__btn--add", function() {
+    $(this).parent().remove();
+  });
+  
 });
