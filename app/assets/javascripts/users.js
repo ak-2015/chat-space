f9e21d07ac35273926c0c974b243bb601800e0b5
ak-2015 <akiko_torii@hotmail.com>
2020-05-18 22:58:31 +0900

    add ajax to js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,15 @@
 $(function() {
   $("#user-search-field").on("keyup", function() {
     let input = $(this).val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+    .done(function(users) {
+    })
+    .fail(function() {
+    });
   });
 });
