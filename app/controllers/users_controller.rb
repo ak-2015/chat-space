841c2a0100afe9618fc2f7829259a5245dd8d39d
ak-2015 <akiko_torii@hotmail.com>
2020-05-20 01:18:08 +0900

    delete extra code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def index
     @users = User.search(params[:keyword], current_user.id)
     respond_to do |format|
-      format.html
       format.json
     end
   end
