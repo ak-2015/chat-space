082bde1f9722b11857e14abd92fb0c5e6ac01b14
ak-2015 <akiko_torii@hotmail.com>
2020-05-21 00:10:26 +0900

    edit codes - done and fail case of reloadMessage function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,10 +9,15 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
       console.log("success");
     })
     .fail(function() {
-      console.log("error");
+      alert('error');
     });
     
   };
