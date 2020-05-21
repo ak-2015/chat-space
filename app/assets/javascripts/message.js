78c5947656b4e0ffd92d6ab0321d8a2fb139714f
ak-2015 <akiko_torii@hotmail.com>
2020-05-21 16:14:08 +0900

    revised codes - where to insert new messages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -14,8 +14,8 @@ $(function(){
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        $('.messages').append(insertHTML);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.chat-main').find('.main').append(insertHTML);
+        $('.chat-main').find('.main').animate({ scrollTop: $('.chat-main').find('.main')[0].scrollHeight});
       }
     })
     .fail(function() {
